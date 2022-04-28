2170492ff5ba7053276ac6993e75b2d6db70c1ba
phvohieunghia <phvohieunghia@gmail.com>
2022-04-28 22:37:05 +0700

    finish highlight textarea

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 import SpinData from './data.json' assert { type: "json" };
 
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const textArea = $('.root .input .container > textarea')
+const backDrop = $('.root .input .container .backdrop')
+const handleSpin = $('.root button')
+const result = $('.result')
+function handleScroll() {
+    textArea.onscroll = function (e) {
+        var scrollTop = textArea.scrollTop;
+        console.log(scrollTop)
+        backDrop.scrollTop = scrollTop;
+    }
+}
+function renderBackdrop(text) {
+
+}
+
 function randomCustom(length) {
     let randomNumber = Math.floor(Math.random() * length)
     return randomNumber
 }
 function Replace(text) {
-    let result = text
+    let result = {
+        origin: '',
+        replace: ''
+    }
+    let replaceText = text
+    let originText = text
     SpinData.forEach(array => {
         array.some((string, i, strings) => {
-            var pretext = result
-            result = result.replace(string, '<span>' + strings[randomCustom(strings.length)] + '</span>')
-            if (pretext !== result) return true
+            var pretext = replaceText
+            replaceText = replaceText.replace(string, '<span>' + strings[randomCustom(strings.length)] + '</span>')
+            originText = originText.replace(string, '<span>' + string + '</span>')
+            if (pretext !== replaceText) return true
         })
     })
-    result = result.replace(/\n/gi, "<br />")
+    replaceText = replaceText.replace(/\n/gi, "<br />")
+    result.replace = replaceText
+
+    originText = originText.replace(/\n/gi, "<br />")
+    result.origin = originText
     return result
 }
 function randomColor() {
@@ -22,16 +50,16 @@ function randomColor() {
     let b = Math.floor(Math.random() * 56 + 200);
     return `rgb(${r},${g},${b})`
 }
-document.querySelector('.root button').onclick = function() {
-    const $ = document.querySelector.bind(document)
-    const $$ = document.querySelectorAll.bind(document)
-    let text = $('.root textarea')
-    
-    let newResult = document.createElement("div")
-    newResult.setAttribute("class", "child")
-    newResult.innerHTML = Replace(text.value)
-    newResult.style.backgroundColor = randomColor()
-    $('.result').prepend(newResult)
+handleSpin.onclick = function() {
+    const _text = $('.root textarea')
+    const text = Replace(_text.value)
+    console.log(text)
+    backDrop.innerHTML = text.origin
+    let newChild = document.createElement("div")
+    newChild.setAttribute("class", "child")
+    newChild.style.backgroundColor = randomColor()
+    newChild.innerHTML = text.replace
+    result.prepend(newChild)
 
     let btnResult = document.createElement('button')
     btnResult.setAttribute("class", "closeBtn")
@@ -42,8 +70,5 @@ document.querySelector('.root button').onclick = function() {
             $$('.result .child')[i].classList.toggle('set-height')
         }
     })
-    // $$('.result .child .closeBtn').onclick = function () {
-    //     $('.result .child').classList.toggle('set-height')
-    // }
 }
-    
+handleScroll()
